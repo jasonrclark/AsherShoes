ad6b967bc8c7120ac2e4bf5bcd43dbd69356421a
Jason R. Clark <jclark@newrelic.com>
2020-04-12 18:23:54 +0200

    Falling blocks reimplementation

--- FallingBlocks.rb
+++ FallingBlocks.rb
@@ -1,21 +1,63 @@
 Shoes.app do
-  falling = []
-  falling << rect(100, 0, 90, 50, fill: blue)
+  @colors = [red, yellow, blue, orange, green, purple]
 
-  rect(0, 450, 10000, 10000, fill: brown)
+  @loops = []
+  @falling = []
+  @ground = rect(0, 450, 10000, 10000, fill: brown)
 
-  every(0.01) do
-    falling.each do |fall|
-      fall.top += 2
+  @loops << every(5) do
+    add_block unless @loops.empty?
+  end
+
+  @loops << every(0.01) do
+    @falling.each do |fall|
+      fall.top += 4
+
+      if (fall.top + fall.height) > @ground.top
+        remove_block(fall)
+        bump_ground
+
+        add_block unless finished?
+      end
+
+      if finished?
+        clean_up
+        para "GAME OVER", top: app.height / 2, size: 48, align: "center"
+      end
     end
   end
 
-  # Blocks different colors
-  # When a block finishes, start another
-  # Random positions for blocks when they start
-  # Draw Ground!!
-  # Ground moves UP when a block touches it
-  # Game ends when ground touches the top of the screen
-  # background city
-  #
+  def add_block
+    left = rand(app.width - 90)
+    new_block = rect(left, 0, 90, 50, fill: @colors.sample)
+
+    new_block.click do
+      remove_block(new_block)
+    end
+
+    @falling << new_block
+  end
+
+  def remove_block(block)
+    @falling.delete(block)
+    block.remove
+  end
+
+  def finished?
+    @ground.top <= 0
+  end
+
+  def bump_ground
+    @ground.top -= 15
+  end
+
+  def clean_up
+    @loops.clear
+
+    @falling.each do |fall|
+      remove_block(fall)
+    end
+  end
+
+  add_block
 end
